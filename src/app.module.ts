dae6a9be2b88749e4addf08b2fa4015030f00c85
jokerf6 <fhakem75@gmail.com>
2023-02-19 18:28:56 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaService } from "./prisma.service";
+import { LoggerMiddleware } from "./Middlewares/Logger";
+import { AuthModule } from "./auth/auth.module";
+import { UploadModule } from './upload/upload.module';
+
+@Module({
+  imports: [AuthModule, UploadModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes("*");
+  }
+}
