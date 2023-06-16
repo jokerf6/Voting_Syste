88ec7ba5e84b0059e06d0b99e063b79a327c9751
jokerf6 <fhakem75@gmail.com>
2023-06-16 03:37:09 +0200

    image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Req, Res, Param } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-@ApiTags()
+  @ApiTags()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/api/v1/uploads/:id")
+  sendFile(@Res() res, @Param("id") id: string) {
+    return this.appService.sendFile(res, id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import path, { join } from "path";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+  async sendFile(res, id) {
+    console.log(id.split("uploads/")[0]);
+
+    const filePath = join(
+      __dirname,
+      "..",
+      `/uploads/${id.split("uploads/")[0]}`
+    );
+
+    res.sendFile(filePath, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Sent:", "fileName");
+      }
+    });
   }
 }
